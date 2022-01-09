6467e33bb26af53ea2f422c9a323ea8554d14399
sandipsky <sandipshakya75@gmail.com>
2022-01-09 10:24:42 +0545

    Second Commit

---
+++ todo/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm, widgets
+from .models import *
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        widgets={
+            'title' : forms.TextInput(attrs={'class':'input'}),
+        }
