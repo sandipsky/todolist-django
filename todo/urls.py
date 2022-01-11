d90e3466b74f5803d19ded098ea4eb6fb9c21363
sandipsky <sandipshakya75@gmail.com>
2022-01-11 10:20:56 +0545

    almost final

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='list'),
+    # path('create/', views.create, name='create'),
     path('update/<str:pk>/', views.update, name='update'),
     path('delete/<str:pk>/', views.delete, name='delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,16 +4,33 @@ from .models import *
 from .forms import *
 # Create your views here.
 def index(request):
+    # tasks = Task.objects.all()
+    count = Task.objects.filter(complete=False).count()
+    # if 'search' in request.GET:
+    #     search = request.GET['search']
+    #     tasks = Task.objects.filter(title__icontains=search)
+    # else:
+    #     tasks = Task.objects.all()
     tasks = Task.objects.all()
     form = TaskForm()
-    count = Task.objects.filter(complete=False).count()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
-    context = {'tasks':tasks, 'form':form, 'count':count}
+    # context = {'tasks':tasks,'count':count, 'search':search}
+    context = {'tasks':tasks,'count':count, 'form': form}
     return render(request, 'index.html', context)
+
+# def create(request):
+#     form = TaskForm()
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         return redirect('/')
+#     context = {'form':form}
+#     return render(request, 'create.html', context)
 
 def update(request, pk):
     task = Task.objects.get(id=pk)
